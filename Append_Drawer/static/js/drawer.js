1ae7f047ed12e4aa400393f19b8cca1442c46074
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-07-08 22:09:55 +0900

    [Update] fin drawer

---
+++ Append_Drawer/static/js/drawer.js
@@ -0,0 +1,49 @@
+/* jQuery, js.cookie */
+
+'use strict';
+
+/* 
+クッキーを動かすにはサーバー環境が必要です。
+ファイルスキーム(file:///)では動きません。
+*/
+const agree = Cookies.get('cookie-agree');
+console.log('agree ', agree);
+if (agree === 'yes') {
+    alert('Cookieを受け入れました');
+} else {
+    alert('Cookieを確認できません');
+}
+
+$(document).ready(function () {
+    
+});
+
+
+$(function () {
+
+    // drawer show
+    $('#open_nav').click(function () {
+        $('#nav').toggleClass('show');
+    });
+
+    // drawer hidden
+    $('#nav').click(function () {
+        $('#nav').toggleClass('show');
+    });
+ 
+    // cookie
+    $('#btn_cookie_agree').click(function () {
+        Cookies.set('cookie-agree', 'yes', { expires: 3 });
+        // window.location.reload(); // htmlページをリロード
+
+        const cookie_panel = document.getElementById('cookie_panel');
+        document.body.removeChild(cookie_panel);
+    });
+
+    // cookie
+    $('#btn_cookie_remove').click(function () {
+        Cookies.remove('cookie-agree');
+        alert('Cookieを削除しました');
+    });
+
+});
